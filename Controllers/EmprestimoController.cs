0b2116bc3ebdfbab9bf8c2bc416faec81e8de405
Alisson Lopes <alyssonboxx33@gmail.com>
2023-07-03 23:54:13 -0300

    Upload Sistema Biblioteca correções e melhorias

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -11,6 +11,7 @@ namespace Biblioteca.Controllers
     {
         public IActionResult Cadastro()
         {
+            Autenticacao.CheckLogin(this);
             LivroService livroService = new LivroService();
             EmprestimoService emprestimoService = new EmprestimoService();
 
@@ -22,21 +23,37 @@ namespace Biblioteca.Controllers
         [HttpPost]
         public IActionResult Cadastro(CadEmprestimoViewModel viewModel)
         {
-            EmprestimoService emprestimoService = new EmprestimoService();
-            
-            if(viewModel.Emprestimo.Id == 0)
+            if(!string.IsNullOrEmpty(viewModel.Emprestimo.NomeUsuario))
             {
-                emprestimoService.Inserir(viewModel.Emprestimo);
+                EmprestimoService emprestimoService = new EmprestimoService();
+                
+                if(viewModel.Emprestimo.Id == 0)
+                {
+                    emprestimoService.Inserir(viewModel.Emprestimo);
+                }
+                else
+                {
+                    emprestimoService.Atualizar(viewModel.Emprestimo);
+                }
+                return RedirectToAction("Listagem");
             }
-            else
-            {
-                emprestimoService.Atualizar(viewModel.Emprestimo);
+            else{
+                ViewData["mensagem"] = "Por favor, preencha todos os campos!";
+
+                LivroService livroService = new LivroService();
+                EmprestimoService emprestimoService = new EmprestimoService();
+
+                CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
+
+                cadModel.Livros = livroService.ListarDisponiveis();
+                
+                return View(cadModel);
             }
-            return RedirectToAction("Listagem");
         }
 
         public IActionResult Listagem(string tipoFiltro, string filtro)
         {
+            Autenticacao.CheckLogin(this);
             FiltrosEmprestimos objFiltro = null;
             if(!string.IsNullOrEmpty(filtro))
             {
@@ -50,6 +67,7 @@ namespace Biblioteca.Controllers
 
         public IActionResult Edicao(int id)
         {
+            Autenticacao.CheckLogin(this);
             LivroService livroService = new LivroService();
             EmprestimoService em = new EmprestimoService();
             Emprestimo e = em.ObterPorId(id);
